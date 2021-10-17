857425943aa1ce0d244d927ef3fe0f69572ff0c4
Laura-Stitt <87922372+Laura-Stitt@users.noreply.github.com>
2021-10-16 21:08:59 -0500

    added styling and removed empty spaces in HTML

---
+++ laura.js
@@ -0,0 +1,18 @@
+// index.html
+
+// changed "due date" to input and added type "date"
+// deleted dropdown menu options
+
+// rows 82, 83 - added image
+// rows 92, 94, 96 - added bootstrap <div class="w-100"></div> for styling
+
+// replaced breaks with bootstrap <div class="w-100"></div>
+
+// bill.js
+
+// reviewed code, no changes necessary
+
+// styles.css
+
+// created background, hover, and img styling for the project
+
